ed47b147ec9ad71bda3273ef014933417c1ed050
janos <risk.limits@gmail.com>
2019-06-07 11:24:11 -0400

    Added a test an iterable that will a read CSV file into memory for blocks

--- src/assemble_sequences.py
+++ src/assemble_sequences.py
@@ -3,17 +3,48 @@ import json
 import argparse
 
 
-class CSVFile(object):
+class CSVBlockFile(object):
 
-    def __init__(self, file_name, block_field_name=None):
+    """Class for reading a CSV file into memory. The assumption is that the file is stored """
+
+    def __init__(self, file_name, block_field_name):
         self.file_name = file_name
 
-        with open(file_name, newline="", mode="r") as f:
-            self.csv_dict = csv.DictReader(f)
+        self.fp = open(file_name, newline="", mode="r")
 
+        self.csv_dict = csv.DictReader(self.fp)
 
+        self.block_field_name = block_field_name
 
+        self.row_i = 0  # Index
+        self.current_block_field_value = None
+        self.past_block_field_value = None
 
+        self.rows_buffer = []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        for row_dict in self.csv_dict:
+            self.row_i += 1
+            self.current_block_field_value = row_dict[self.block_field_name]
+
+            if self.past_block_field_value is None or self.past_block_field_value == self.current_block_field_value:
+                self.rows_buffer += [row_dict]
+                self.past_block_field_value = self.current_block_field_value
+            else:
+                self.past_block_field_value = self.current_block_field_value
+                rows_dict = list(self.rows_buffer)
+                self.rows_buffer = [rows_dict]
+                return rows_dict
+
+        if len(self.rows_buffer):
+            rows_dict = list(self.rows_buffer)
+            self.rows_buffer = []
+            return rows_dict
+        else:
+            raise StopIteration
 
 
 def main(json_file_assembly_mapping, input_directory, output_file_name):
--- test/test_assemble_sequences.py
+++ test/test_assemble_sequences.py
@@ -1,6 +1,23 @@
 import unittest
+import pprint
 
-from assemble_sequences import main
+from assemble_sequences import main, CSVBlockFile
+
+
+class CSVBlockFileTestCase(unittest.TestCase):
+    def test_read(self):
+
+        block_obj = CSVBlockFile("./data/measurements.csv", "encounter_id")
+
+        list_block_obj = list(block_obj)
+
+        self.assertEqual(3, len(list_block_obj))
+
+        self.assertEqual(6, len(list_block_obj[0]))
+
+        self.assertEqual(2, len(list_block_obj[1]))
+
+        self.assertEqual(1, len(list_block_obj[2]))
 
 class AssembleTestCase(unittest.TestCase):
     def test_assemble(self):
